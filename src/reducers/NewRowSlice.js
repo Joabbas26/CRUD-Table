2d590767813913150a7049bf9958752e6ebb885c
Joabbastidas <joabbastiddas@gmail.com>
2022-05-06 09:50:40 -0500

    Clean up code

--- src/reducers/NewRowSlice.js
+++ src/reducers/NewRowSlice.js
@@ -19,24 +19,20 @@ const NewRowSlice = createSlice({
         },
         deleteRow: (state, action) => {
             state.splice(action.payload.rowNum - 1, 1);
-            },
-            saveRow: (state, action) => {
-                const updatedRow = {
-                    // rowNum: action.payload.rowNum,
-                    fName: action.payload.fName,
-                    lName: action.payload.lName,
-                    looks: action.payload.looks,
-                    make: action.payload.make,
-                    eng: action.payload.eng,
-                    recomm: action.payload.recomm,
-                    total: action.payload.total,
-                };
-                // const index = state.findIndex((rows) =>
-                    // rows.id === action.payload.id);
-                    // state.splice(action.payload.rowNum, 1, updatedRow);
-                    state.splice(state.findIndex((arrow) => arrow.id === action.payload), 1, updatedRow);
-
-            },
+        },
+        saveRow: (state, action) => {
+            const updatedRow = {
+                // rowNum: action.payload.rowNum,
+                fName: action.payload.fName,
+                lName: action.payload.lName,
+                looks: action.payload.looks,
+                make: action.payload.make,
+                eng: action.payload.eng,
+                recomm: action.payload.recomm,
+                total: action.payload.total,
+            };
+            state.splice(state.findIndex((arrow) => arrow.id === action.payload), 1, updatedRow);
+        },
     },
 });
 
@@ -46,31 +42,14 @@ export const { addRow, deleteRow, saveRow } = NewRowSlice.actions;
 /*
 
 
-
+const index = state.findIndex((rows) =>
+    rows.id === action.payload.id);
+    state.splice(action.payload.rowNum, 1, updatedRow);
 
 
 deleteMeal: (state, action) => {
-    // you receive you inputIndex from the payload
     let { inputIndex } = action.payload;
-    // and you use it to splice the desired item off the array
     state.meals.splice(inputIndex, 1);
-    ...your other logic if any
 },
-
-var i = remove.length;
-while (i--) {
-    if (remove[i] != undefined)
-        options.series[0].data.splice(remove[i],1);
-}  
-remove.push(index);
-
-var cache = ["0", "1", "2", "3", "4"];
-cache.r = "r";
-console.log(cache.length);
-for (var i = cache.length - 1; i >= 0; i--) {
-    cache.splice(i, 1);
-}
-console.log(cache);
-
 
 */
